a953375a9fc41b9d0ffb7ce0bfd8acb621332a7e
Sabirul1127 <sislam1127@gmailcom>
2026-06-26 18:28:37 -0400

    Add login and registration endpoint

--- backend/backend/app/database.py
+++ backend/backend/app/database.py
@@ -2,7 +2,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:YOUR_PASSWORD@localhost/helpdesk_db"
+DATABASE_URL = "postgresql://postgres:Nadiashafik1@localhost/helpdesk_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
@@ -10,3 +10,15 @@ SessionLocal = sessionmaker(
     autoflush=False,
     bind=engine
 )
+
+
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
